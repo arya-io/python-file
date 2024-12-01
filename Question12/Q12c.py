4c9031cd920c85640231d239c7757f1819d0875c
Arya <aryanoble06@gmail.com>
2024-12-01 12:45:41 +0530

    Day 3

---
+++ Question12/Q12c.py
@@ -0,0 +1,10 @@
+# Write a program, which will find all such numbers between 1000 and 3000 (both included) such that 
+# each digit of the number is an even number.
+# The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+def check(element):
+    return all(ord(i)%2 == 0 for i in element)  # all returns True if all digits i is even in element
+
+lst = [str(i) for i in range(1000,3001)]        # creates list of all given numbers with string data type
+lst = list(filter(check,lst))                   # filter removes element from list if check condition fails
+print(",".join(lst))
---
+++ Question12/Q12e.py
@@ -0,0 +1,7 @@
+# Write a program, which will find all such numbers between 1000 and 3000 (both included) such that 
+# each digit of the number is an even number.
+# The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+# map() digits of each number with lambda function and check if all() of them even
+# str(num) gives us opportunity to iterate through number by map() and join()
+print(','.join([str(num) for num in range(1000, 3001) if all(map(lambda num: int(num) % 2 == 0, str(num)))]))
