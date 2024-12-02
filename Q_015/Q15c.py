46a697288337ca1a3fe5cc2e514e787603d66ba2
Arya <aryanoble06@gmail.com>
2024-12-02 19:06:29 +0530

    Day 4

---
+++ Q_015/Q15c.py
@@ -0,0 +1,12 @@
+# Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+
+# Suppose the following input is supplied to the program:
+
+# 9
+# Then, the output should be:
+
+# 11106
+
+a = input()
+total = int(a) + int(2*a) + int(3*a) + int(4*a)  # N*a=Na, for example  a="23", 2*a="2323",3*a="232323"
+print(total)
