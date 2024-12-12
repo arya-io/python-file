10578cf0ab09e371daad909ef2b78131f33f5cf2
Arya <aryanoble06@gmail.com>
2024-12-13 02:08:19 +0530

    Day 14

---
+++ Q_052/Q52a.py
@@ -0,0 +1,15 @@
+# Define a custom exception class which takes a string message as attribute.
+
+class MyError(Exception):
+    """My own exception class
+
+    Attributes:
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, msg):
+        self.msg = msg
+
+error = MyError("something wrong")
+
+print(error)
