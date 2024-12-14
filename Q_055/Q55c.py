3bbe24657698d09bdb5467a9715608bdacd2b07c
Arya <aryanoble06@gmail.com>
2024-12-14 21:37:31 +0530

    Day 15

---
+++ Q_055/Q55c.py
@@ -0,0 +1,16 @@
+# Write a program which accepts a sequence of words separated by whitespace as input 
+# to print the words composed of digits only.
+
+# Example: If the following words is given as input to the program:
+
+# 2 cats and 3 dogs.
+# Then, the output of the program should be:
+
+# ['2', '3']
+
+email = input().split()
+ans = []
+for word in email:
+    if word.isdigit():     # can also use isnumeric() / isdecimal() function instead
+       ans.append(word)
+print(ans)
